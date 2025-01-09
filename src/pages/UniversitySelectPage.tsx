3819fbaa28ec170db19a9cfc93e72fed4155131c
박현우 <gusdn3477@naver.com>
2025-01-09 20:17:48 +0900

    fix: useCallback 인자에 selectUniv 넣어줘서 postMessage payload 제대로 가도록 수정

--- src/pages/UniversitySelectPage.tsx
+++ src/pages/UniversitySelectPage.tsx
@@ -31,7 +31,7 @@ export default function UniversitySelectPage() {
                 university: selectedUniv.university
             }
         }));
-    }, [])
+    }, [selectedUniv])
 
     const handleKeywordChange = useCallback((keyword: string) => {
         setKeyword(keyword);
@@ -42,19 +42,19 @@ export default function UniversitySelectPage() {
     };
 
     return (
-        <div className='p-4 w-full'>
-            <header className='flex items-center h-[24px]'>
+        <div className='p-3 w-full'>
+            <header className='flex items-center h-[20px]'>
                 <BackButton onClick={handleBack} />
             </header>
             <main className="h-[calc(100%-120px)]">
-                <h2 className="mt-4 text-[24px]">세종대학교 재학생이신가요?</h2>
+                <h2 className="mt-7 text-[24px]">세종대학교 재학생이신가요?</h2>
                 <span>학교 이름이 버디야 프로필에 적혀요.</span>
                 <div className="mt-[28px] h-full">
                     <SearchBar keyword={keyword} onChange={handleKeywordChange} />
                     <UnivList keyword={keyword} selectedUnivId={selectedUniv.id} onSelect={handleSelectUniv} />
                 </div>
             </main>
-            <button className='flex items-center justify-center mt-1 w-full h-[60px] rounded-[12px] bg-primary' onClick={handleButtonClick} disabled={selectedUniv.id === 0}>
+            <button className='flex items-center justify-center mt-3 w-full h-[60px] rounded-[12px] bg-primary' onClick={handleButtonClick} disabled={selectedUniv.id === 0}>
                 <span className="text-white">{'다음'}</span>
             </button>
         </div >
