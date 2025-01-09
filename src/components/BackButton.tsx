784aaa754fa4da3cc0bed4279ea38c58112498e5
박현우 <gusdn3477@naver.com>
2025-01-09 20:17:15 +0900

    style: backButton width, height 수정

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -9,7 +9,7 @@ export default function BackButton({ onClick }: BackButtonProps) {
     }
     return (
         <button onClick={handleButtonClick}>
-            <svg width="30" height="31" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="28" height="28" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M18.7416 24.3649L10.0293 15.6527L18.7416 6.94043" stroke="#282828" strokeWidth="2.48922" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
         </button>
